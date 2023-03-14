af48e2e3508d1125951338faf21bb45f318f39ae
Jimmy Lindqvist <jimmylindqvist@Jimmy.local>
2023-03-14 12:27:45 +0700

    Added the  files to github

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,18 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./dist/**/*.html"],
   theme: {
+    screens:{
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')
   ],
 }
